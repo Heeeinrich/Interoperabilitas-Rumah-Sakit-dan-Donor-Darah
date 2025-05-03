08c529dbf7d3eaba832d2aa255fca183d8a9bad5
Heinrich Radhitya Gavrilla Atmaja <heinrichradhityagavr@mail.ugm.ac.id>
2025-05-03 10:33:18 +0700

    Add files via upload

---
+++ Sistem Informasi Rumah Sakit/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '777fda46b77a820977a5ba616283dbfbc40bf7e1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '777fda46b77a820977a5ba616283dbfbc40bf7e1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
